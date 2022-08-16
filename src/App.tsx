dc5a8791694c9b27fd509cb61316dbbad17c213a
Gordao-dev <gorda-dev@pop-os.localdomain>
2022-08-16 00:08:26 -0300

    add task by clicking enter

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import * as C from './App.styles';
 import { Item } from './types/item';
 import { ListItem } from './components/Listitem';
+import { AddArea } from './components/AddArea';
 
 const App = () => {
   const [list, setList] = useState<Item[]>([
@@ -9,12 +10,23 @@ const App = () => {
     { id: 24, name: 'Validar Bilhetes', done: true },
   ]);
 
+  const handleAddTask = (taskName: string) => {
+    let newList = [...list];
+    newList.push({
+      id: list.length + 1,
+      name: taskName,
+      done: false
+    });
+    setList(newList);  }
+
   return (
       <C.Container>
          <C.Area>
             <C.Header> Lista de Tarefas </C.Header>
 
             {/* Area for add new tasks */}
+
+            <AddArea onEnter={handleAddTask} />
 
             {/* Task List */}
 
